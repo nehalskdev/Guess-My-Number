309ba5f3b42eeb2a2abcdcd825a9369b18ca05dc
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-05-06 17:10:17 +0530

    pushing html and css

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+// console.log (document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 15;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
+
+
+
+
+
