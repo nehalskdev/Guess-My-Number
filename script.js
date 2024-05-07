67aead5cac61c3cc6eb10f865d66bb8967ee9edc
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-05-07 11:57:42 +0530

    added if else

--- script.js
+++ script.js
@@ -21,17 +21,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low!!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
-
-
-
-
-
