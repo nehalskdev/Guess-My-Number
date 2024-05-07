0075f15d530c1d9707beb15a3e4aad1add43a79f
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-05-07 12:18:39 +0530

    added btn functionality

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
 // document.querySelector('.guess').value = 15;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let  secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -51,3 +52,12 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = "?";
+  document.querySelector('.guess').value = '';
+})
